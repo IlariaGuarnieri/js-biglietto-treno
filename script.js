2ca2fa9c7f4224d4e3d8c1d6a8b2e4049bab88ed
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-02-20 17:30:49 +0100

    [edit] prove per calcolare il prezzo del biglietto

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
+//definizione
+let nomeInventato1;
+var nomeInvetato2;
+
+//assegnamento
+nomeInventato1 = "paolo";
+nomeInvetato2 = 5;
+
+//inizializzazione = definizion + assegnamento (in una unica riga)
+const altroNomeInventato = "hello world"; 
+
+// ********** inizio **********
+
 const numKm =prompt ('Quanti km vuoi percorrere?');
 console.log (numKm);
 
-const age =prompt ('Quanti anni hai? ');
-console.log (age);
+const etaPasseggero =prompt ('Quanti anni hai? ');
+console.log (etaPasseggero);
 
 const prezzoPerKm = 0.21
-let prezzoTotale = distanza * prezzoPerKm
 
-let message = '';
+const prezzoTotale = numKm * prezzoPerKm
+console.log (prezzoTotale)
 
-if(userAge < 18){
+// let message = '';
+
+
+if(etaPasseggero < 18){
   message = 'Utente minorenne ha sconto 20%'
-}else if (userAge > 65){
+}else if (etaPasseggero > 65){
   message = 'Utente senior ha sconto 40%'
 }else{
   message = 'utente non ha sconto'
